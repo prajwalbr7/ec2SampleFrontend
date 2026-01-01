8294a8c4d6da260ccd022092010fe08063ef14ef
Prajwal yadav <prajwalbr7@gmail.com>
2026-01-01 22:00:23 +0530

    commited

---
+++ form/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://13.232.252.174:5000/submit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    });
+    const data = await res.json();
+    setResponse(data.message);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Form Data</h2>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{response}</p>
+    </div>
+  );
+}
+
+export default App;
